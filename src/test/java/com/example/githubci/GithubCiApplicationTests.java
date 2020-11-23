6c53198c06943163e854156b81a7ef4cd3ccc138
zhamzaoui <zakaria.hamzaoui-ext@cdgcapital.ma>
2020-11-23 15:33:44 +0100

    first commit

---
+++ src/test/java/com/example/githubci/GithubCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
